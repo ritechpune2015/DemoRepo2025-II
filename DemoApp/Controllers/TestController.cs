ac71e038076d3ef74d04cdc0c63012853374ee06
ritech <contact@ritechpune.com>
2025-04-06 11:51:53 +0530

    test controller index acton added with view

---
+++ DemoApp/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
